7e1be36cdb22466a0cf61d979a2c9135f17defdf
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-13 14:47:13 +0530

    33 FlatList Horizontal And Vertical Scrolling

--- src/screens/FlatListDemo.jsx
+++ src/screens/FlatListDemo.jsx
@@ -5,34 +5,59 @@ import { FlatList } from 'react-native-web'
 const FlatListDemo = () => {
     const names = [
         {
+            index: "1",
             name: "Deepak"
         },
         {
+            index: "2",
             name: "Pradeep"
         },
         {
+            index: "3",
             name: "Vikram"
         },
         {
+            index: "4",
             name: "Rahul"
         },
         {
+            index: "5",
             name: "Jabba"
+        },
+        {
+            index: "6",
+            name: "Deepak"
         },
     ]
     return (
-        <FlatList data={names} renderItem={(element) => {
-            console.log(element.item.name);
-            return (
-                <Text style={styles.textStyles}>{element.item.name}</Text>
-            )
-        }} />
+        <FlatList style={styles.listStyle} keyExtractor={(key) => {
+            return key.index
+        }}
+            horizontal
+            inverted
+            // numColumns={2}
+            showHorizontalScrollIndicator={false}
+            data={names} renderItem={(element) => {
+                console.log(element.item.name);
+                return (
+                    <Text style={styles.textStyles}>{element.item.name}</Text>
+                )
+            }} />
     )
 }
 
 const styles = StyleSheet.create({
     textStyles: {
         fontSize: 40,
+        padding: 30,
+        backgroundColor: 'blue',
+        margin: 20,
+        color: "white",
+    },
+    listStyle: {
+        textAlign: "center",
+        margin: 20,
+        padding: 10
     }
 })
 
