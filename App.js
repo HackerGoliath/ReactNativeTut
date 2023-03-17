1028f6f8b61f5ea3c05f50df81e6899804a9002f
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-17 16:14:23 +0530

    9 Online Education App

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 // import libraries to create compenents
-import { StyleSheet, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, View, Image, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Contact from './src/screens/Form/Contact';
-import Home from './src/screens/Form/Home';
+import Home from './src/Projects/OnlineEducationSystem/Home';
+// import Contact from './src/screens/Form/Contact';
+// import Home from './src/screens/Form/Home';
 // import HookEffect from './src/screens/HookEffect';
 // import ColorGenerator from './src/Projects/ColorGenerator';
 // import CounterNumber from './src/screens/CounterNumber';
@@ -22,6 +23,7 @@ import Home from './src/screens/Form/Home';
 // create a component that return some jsx / simple function
 const App = () => {
   const Stack = createNativeStackNavigator();
+  const description = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae dolorem modi consequatur nihil quis praesentium voluptas quasi? Facilis cum, voluptas maiores iusto ipsa eos libero porro optio suscipit sint praesentium soluta culpa accusantium eaque.";
   // const myName = "Deepak";
 
   // const myElement = <Text>Hello World! {myName} from React Native from element</Text>
@@ -34,43 +36,62 @@ const App = () => {
     //    <Text style={styles.textStyle}>Hello World! {myName} from React Native from App</Text>
     //   <CustomComp />
     //   {myElement}
-    //   <Text>Hi my full name is : {getFullName("Deepak", "Bhai", "Banarasi")}</Text> 
-    //   ==================================================================================================== 
+    //   <Text>Hi my full name is : {getFullName("Deepak", "Bhai", "Banarasi")}</Text>
+    //   ====================================================================================================
 
-    //   Challenge-1 Show Three Different Paragraphs 
-    //   <FirstChallenge /> 
+    //   Challenge-1 Show Three Different Paragraphs
+    //   <FirstChallenge />
 
-    //     FlatList 
-    //   <FlatListDemo /> 
+    //     FlatList
+    //   <FlatListDemo />
 
-    //    Images and Props 
-    //    <Images /> 
-    //  Buttons and TouchableOpacity 
-    //    <ButtonDemo /> 
-    //  Card 
-    //    <NetflixCard /> 
-    //  Box Model 
-    //    <BoxModel /> 
-    //  Challenge-2 Scrolling Effects FlatList 
-    //    <Challeng2SrollingEffects /> 
-    //  FlexBox 
-    //    <FlexBox /> 
-    //  Positions 
-    //    <Positions /> 
-    //  UseState Hook 
-    //    <CounterNumber /> 
-    //  Project-1: Random Color Generator 
-    //    <ColorGenerator /> 
-    //  useEffect Hook 
-    //    <HookEffect /> 
-    //  React Navigation v6 
-    //    Form Login 
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name="Login" component={Contact} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    //    Images and Props
+    //    <Images />
+    //  Buttons and TouchableOpacity
+    //    <ButtonDemo />
+    //  Card
+    //    <NetflixCard />
+    //  Box Model
+    //    <BoxModel />
+    //  Challenge-2 Scrolling Effects FlatList
+    //    <Challeng2SrollingEffects />
+    //  FlexBox
+    //    <FlexBox />
+    //  Positions
+    //    <Positions />
+    //  UseState Hook
+    //    <CounterNumber />
+    //  Project-1: Random Color Generator
+    //    <ColorGenerator />
+    //  useEffect Hook
+    //    <HookEffect />
+    //  React Navigation v6
+    //    Form Login
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName='Login'>
+    //     <Stack.Screen name="Login" component={Contact} />
+    //     <Stack.Screen name="Home" component={Home} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+
+    // Project: Online Education System
+    <View style={styles.mainContainer}>
+      <View style={styles.homeTop}>
+        <Image style={styles.headerImage}
+          resizeMode="contain"
+          source={require('./assets/logo.jpg')} />
+        <Text style={styles.mainHeader}>Welcome to</Text>
+        <Text style={[styles.mainHeader,
+        {
+          fontSize: 33,
+          color: "#4c5dab",
+          marginTop: 0,
+        },
+        ]}>Deepak Technical</Text>
+        <Text style={styles.paraStyle}>{description}</Text>
+      </View>
+    </View>
 
     // </View>
   );
@@ -78,14 +99,51 @@ const App = () => {
 
 // Create a stylesheet to style the component
 const styles = StyleSheet.create({
-  container: {
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#fff',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // textStyle: {
+  //   color: "red",
+  // },
+
+
+  // Project Style:
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    textAlign: "center",
   },
-  textStyle: {
-    color: "red",
+  homeTop: {
+    flex: 1,
+    // justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  headerImage: {
+    height: undefined,
+    width: "100%",
+    aspectRatio: 1,
+    borderRadius: 20,
+    marginTop: 50,
+  },
+  mainHeader: {
+    fontSize: 30,
+    color: "#344055",
+    textTransform: "uppercase",
+  },
+  paraStyle: {
+    textAlign: "left",
+    fontSize: 19,
+    color: "#7d7d7d",
+    marginTop: 30,
+    paddingBottom: 50,
+    lineHeight: 26,
   },
 });
 
